250b9375aef27f8cbb71ef66f5347064a7ab7e7a
Carlos Macaneta <carlosmacaneta@hotmail.com>
2021-10-31 15:28:15 +0200

    Product (create)  and request validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function store(ProductRequest $request): JsonResponse
+    {
+        Product::create($request->all());
+
+        return response()->json(['message' => 'Product added successfully.'], 200);
+    }
 }
