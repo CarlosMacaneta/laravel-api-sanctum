1f9947f7f82236b57f4e80cfb1e74d922c9e6966
Carlos Macaneta <carlosmacaneta@hotmail.com>
2021-10-31 13:06:53 +0200

    Initial commit (Produt - model, controller, migration added)

---
+++ database/migrations/2021_10_31_105609_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128)->nullable(false);
+            $table->string('description', 128)->nullable(false);
+            $table->decimal('price', 7,2, true)->default(0)->nullable(false);
+            $table->integer('qtd', false, true)->default(0)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
