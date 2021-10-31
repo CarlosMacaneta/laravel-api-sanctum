6876d5d03b709a5aa468c2fe48a990fe5d07b160
Carlos Macaneta <carlosmacaneta@hotmail.com>
2021-10-31 16:29:55 +0200

    Product: fetching single record, update and delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,15 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProductRequest;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(): JsonResponse
     {
-        $products = Product::all();
-
-        return response()->json(['products' => $products], 200);
+        return response()->json(['products' =>  Product::all()], 200);
     }
 
     public function store(ProductRequest $request): JsonResponse
@@ -21,5 +18,31 @@ class ProductController extends Controller
         Product::create($request->all());
 
         return response()->json(['message' => 'Product added successfully.'], 200);
+    }
+
+    public function show($id): JsonResponse
+    {
+        if (!$product = Product::find($id)){
+            return response()->json(['message' => 'No product found'], 404);
+        }
+        return response()->json(['products' => $product], 200);
+    }
+
+    public function update(ProductRequest $request, $id): JsonResponse
+    {
+        if (!$product = Product::find($id)){
+            return response()->json(['message' => 'No product found'], 404);
+        }
+        $product->update($request->all());
+        return response()->json(['message' => 'Product updated successfully'], 200);
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        if (!$product = Product::find($id)){
+            return response()->json(['message' => 'No product found'], 404);
+        }
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('products', [ProductController::class, 'index'])->name('products.index');
 Route::post('product/add', [ProductController::class, 'store'])->name('products.store');
-
+Route::get('product/{id}/show', [ProductController::class, 'show'])->name('products.show');
+Route::put('product/{id}/update', [ProductController::class, 'update'])->name('products.update');
+Route::delete('product/{id}/delete', [ProductController::class, 'destroy'])->name('product.destroy');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
