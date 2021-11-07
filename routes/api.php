8e77ca177fd9e3684caa0f503bb7b4abddae0b82
Carlos Macaneta <carlosmacaneta@hotmail.com>
2021-11-08 01:03:58 +0200

    Routes api clean-up

--- routes/api.php
+++ routes/api.php
@@ -16,20 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::get('products', [ProductController::class, 'index'])->name('products.index');
-Route::post('product/add', [ProductController::class, 'store'])->name('products.store');
-Route::get('product/{id}/show', [ProductController::class, 'show'])->name('products.show');
-Route::put('product/{id}/update', [ProductController::class, 'update'])->name('products.update');
-Route::delete('product/{id}/delete', [ProductController::class, 'destroy'])->name('product.destroy');
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-}); */
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::middleware(['auth', 'sanctum'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
     Route::resource('products', ProductController::class);
